3d6e95673d80a3e706c3a03646aad0b746255abf
Maddie Wagner <musomaddy@gmail.com>
2019-10-14 15:20:38 +1100

    Created the actual database file

---
+++ CrossStitchOrganiser/app/src/main/java/project/comp5216/crossstitchorganiser/OrganiserDatabase.java
@@ -0,0 +1,30 @@
+package project.comp5216.crossstitchorganiser;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {ThreadDatabaseItem.class}, version=1)
+
+public abstract class OrganiserDatabase extends RoomDatabase {
+    private static final String DATABASE_NAME = "organiser_db";
+    private static OrganiserDatabase DBINSTANCE;
+
+    public abstract ThreadDao threadDao();
+
+    public static OrganiserDatabase getDatabase(Context context) {
+        if (DBINSTANCE == null) {
+            synchronized (OrganiserDatabase.class) {
+                DBINSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                        OrganiserDatabase.class, DATABASE_NAME).build();
+            }
+        }
+        return DBINSTANCE;
+    }
+
+    public static void destroyInstance() {
+        DBINSTANCE = null;
+    }
+}

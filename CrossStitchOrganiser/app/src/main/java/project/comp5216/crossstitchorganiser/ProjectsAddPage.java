6b61ac56e55af61cae473ef760d7efa80c999f93
Maddie Wagner <maddiewagner@192-168-1-110.tpgi.com.au>
2019-10-19 18:46:23 +1100

    Linear View for threads add now displays somewhat dynamically

--- CrossStitchOrganiser/app/src/main/java/project/comp5216/crossstitchorganiser/ProjectsAddPage.java
+++ CrossStitchOrganiser/app/src/main/java/project/comp5216/crossstitchorganiser/ProjectsAddPage.java
@@ -4,9 +4,14 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProjectsAddPage extends Activity {
 
@@ -14,6 +19,7 @@ public class ProjectsAddPage extends Activity {
 	
 	private String projectTitle;
 	private Project newProject;
+	private List<ThreadDetails> threadDetails;
 
     private ProjectDao projectDao;
 	private OrganiserDatabase db;
@@ -22,13 +28,26 @@ public class ProjectsAddPage extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projects_add);
         Log.v(APP_TAG, "Loading Projects Add Page");
+
+        // Loading the dynamic thread amount adding!
+        LinearLayout ll = (LinearLayout) findViewById(R.id.projectsAddThreadDets);
+        for (int i = 0; i < 3; i++) {
+			View view = LayoutInflater.from(this).inflate(R.layout.view_add_thread_to_project, null);
+			// TODO: the edit text info
+			EditText dmcET = view.findViewById(R.id.projectsAddThreadDmc);
+			EditText amountET = view.findViewById(R.id.projectsAddThreadAmount);
+			threadDetails.add(new ThreadDetails(dmcET, amountET);
+			ll.addView(view);
+		}
+
+        threadDetails = new ArrayList<ThreadDetails>();
 
 		// Loading the database
 		db = OrganiserDatabase.getDatabase(this.getApplication().getApplicationContext());
 		projectDao = db.projectDao();
 
-        setContentView(R.layout.activity_projects_add);
     }
 
 	public void onProjectsAddSubmitClick(View view) {
@@ -60,5 +79,26 @@ public class ProjectsAddPage extends Activity {
                 return null;
             }
         }.execute();
+	}
+}
+
+class ThreadDetails {
+
+	// TODO: fix up this class (it's own file??)
+	// needs to store the edit text for each 
+	private EditText dmcET;
+	private EditText amountET;
+
+	public ThreadDetails(EditText dmc, EditText amount) {
+		this.dmcET = dmc;
+		this.amountET = amount;
+	}
+
+	public EditText getDmcET() {
+		return this.dmcET;
+	}
+
+	public EditText getAmountET() {
+		return this.amountET;
 	}
 }

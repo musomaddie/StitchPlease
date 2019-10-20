dfb44f0a83a9bc05142a94ba1a0e9348501c0bae
Maddie Wagner <maddiewagner@192-168-1-110.tpgi.com.au>
2019-10-20 13:41:19 +1100

    Can now list all wishlist projects successfully

--- CrossStitchOrganiser/app/src/main/java/project/comp5216/crossstitchorganiser/ProjectsWishlistPage.java
+++ CrossStitchOrganiser/app/src/main/java/project/comp5216/crossstitchorganiser/ProjectsWishlistPage.java
@@ -2,6 +2,7 @@ package project.comp5216.crossstitchorganiser;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,19 +24,27 @@ public class ProjectsWishlistPage extends Activity {
     private List<Project> wishlistProjects;
     private ArrayAdapter<Project> projectAdapter;
 
+    private ProjectDao projectDao;
+	private OrganiserDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_projects_wishlist);
         Log.v(APP_TAG, "Loading projects wishlist");
 
-        // Gonna create project list now
-        listView = findViewById(R.id.projectsWishlistList);
-        setUpProjectData();
-        //projectAdapter = new ProjectAdapter(this, wishlistProjects, "AHH");
-        //listView.setAdapter(projectAdapter);
+        // Setting up the database
+		db = OrganiserDatabase.getDatabase(this.getApplication().getApplicationContext());
+		projectDao = db.projectDao();
 
+		loadWishlistProjects();
+
+		// Setting up list view
+        listView = (ListView) findViewById(R.id.projectsWishlistList);
+        projectAdapter = new ProjectAdapter(this, wishlistProjects);
+        listView.setAdapter(projectAdapter);
         setUpProjectItemListener();
+
     }
 
     public void onProjectsWishlistBackClick(View view) {
@@ -61,13 +70,29 @@ public class ProjectsWishlistPage extends Activity {
         });
     }
 
-    private void setUpProjectData() {
-        // TODO: this is dummy data: read from the database properly later
-        wishlistProjects = new ArrayList<Project>();
-        wishlistProjects.add(new Project("PROJECT 1", true));
-        Map<Thread, Double> t = new HashMap<Thread, Double>();
-        t.put(new Thread("310", Colour.BLACK, 1.1), 1.0);
-        t.put(new Thread("550", Colour.BLUE, 1.2), 0.2);
-       // wishlistProjects.add(new Project("PROJECT 2", t, true));
-    }
+	private void loadWishlistProjects() {
+        try {
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... voids) {
+                	List<ProjectDatabaseItem> itemsFromDB = projectDao.listAll();
+                	wishlistProjects = new ArrayList<Project>();
+                    if (itemsFromDB != null && itemsFromDB.size() > 0) {
+                    	for (ProjectDatabaseItem item: itemsFromDB) {
+                    		// For display in list view, we don't care about the
+                    		// thread information
+                    		Project project = new Project(item.getTitle(), item.isWishlist(), item.getPicturePath());
+                    		if (project.isWishlist()) {
+                    			wishlistProjects.add(project);
+							}
+							Log.i(APP_TAG, "Read item from database: " + item.getTitle());
+                        }
+                    }
+                    return null;
+                }
+            }.execute().get();
+        } catch(Exception ex) {
+			Log.e(APP_TAG, ex.getStackTrace().toString());
+        }
+	}
 }

01ae0325541a27943348dd1097b13bf55ee288f3
MadeleineWagner <madeleinewagner@localhost.localdomain>
2020-12-06 18:14:58 +1100

    Modified database to be created from a schema

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,8 +1,30 @@
-# import click
+import click
 import sqlite3
 
 from flask import current_app, g
-# from flask.cli import with_appcontext
+from flask.cli import with_appcontext
+
+
+def close_db(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf8"))
+
+
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    """ Clear the existing data and add new tables """
+    init_db()
+    click.echo("Initialised the database")
 
 
 def get_db():
@@ -14,10 +36,3 @@ def get_db():
         g.db.row_factory = sqlite3.Row
 
     return g.db
-
-
-def close_db(e=None):
-    db = g.pop("db", None)
-
-    if db is not None:
-        db.close()

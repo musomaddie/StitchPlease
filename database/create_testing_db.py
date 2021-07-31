4374c5f235869402718fcc613b1435cfef87cb6b
Maddie Wagner <musomaddy@gmail.com>
2021-07-31 17:03:51 +1000

    Adding a database for testing

---
+++ database/create_testing_db.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+DB_NAME = "testing.db"
+
+db = sqlite3.connect(DB_NAME)
+cursor = db.cursor()
+with open("schema.sql") as f:
+    cursor.executescript(f.read())
+
+db.commit()
+db.close()

3b7f75d341b2250130ad9ba79209138bd0f1eee0
musomaddie <musomaddy@gmail.com>
2021-08-01 16:52:14 +1000

    Finished setting up all the framework for testing. Should now be able to test successfully

--- database_api/stitch_please_database/auth.py
+++ database_api/stitch_please_database/auth.py
@@ -4,7 +4,7 @@ from flask import(
     Blueprint, flash, g, redirect, render_template, request, session, url_for)
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from database.db import get_db
+from stitch_please_database.db import get_db
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
---
+++ database_api/tests/test_threads.py
@@ -0,0 +1,7 @@
+import pytest
+from flask import g, session
+from stitch_please_database.db import get_db
+
+
+def test_example_test(client):
+    assert True
